d4708aaffc40325c9e7455020406dafbaaa38425
Hassan Fazeel <fazeeel@gmail.com>
2023-01-30 00:24:37 +0500

    breeze package updated

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\RoleController;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\PermissionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,31 +13,19 @@ use App\Http\Controllers\PermissionController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
 Route::get('/', function () {
     return view('welcome');
 });
-//Route::get('/users/{user}', [UserController::class, 'show']);
-
-route::resource('/roles',RoleController::class)->middleware(['auth']);
-route::resource('/users',UserController::class)->middleware(['auth']);
-route::resource('/permissions',PermissionController::class)->middleware(['auth']);
-
-route::get('/permissions/{permission}/assign', [PermissionController::class,'assign'])->name('permissions.assign');//to assign roles
-route::post('/permissions/{permission}/', [PermissionController::class,'sync'])->name('permissions.sync');//to assign roles (post)
-
-route::get('/roles/{role}/assign', [RoleController::class,'assign'])->name('roles.assign');//to assign Permission
-route::post('/roles/{role}/', [RoleController::class,'sync'])->name('roles.sync');//to assign permission (post)
-
-
-route::get('/users/{user}/assignrole',[UserController::class, 'assignrole'])->name('users.assignrole');//assign users to role
-route::post('/users/{user}/role',[UserController::class, 'syncrole'])->name('users.syncrole');//assign users to role
-
-route::get('/users/{user}/assignpermission',[UserController::class, 'assignpermission'])->name('users.assignpermission');//assign users to role
-route::post('/users/{user}/permission',[UserController::class, 'syncpermission'])->name('users.syncpermission');//assign users to role
 
 Route::get('/dashboard', function () {
     return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-})->middleware(['auth'])->name('dashboard');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
 require __DIR__.'/auth.php';
