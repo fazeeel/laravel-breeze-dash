a89ef3714b6aa5c34abba0e5a2f1b1bca9b863be
Hassan Fazeel <fazeeel@gmail.com>
2023-01-30 00:35:58 +0500

    roles and permission routes updated after breeze package update

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,11 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PermissionController;
 
+/*
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -28,4 +32,21 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+
+route::resource('/roles',RoleController::class)->middleware(['auth']);
+route::resource('/users',UserController::class)->middleware(['auth']);
+route::resource('/permissions',PermissionController::class)->middleware(['auth']);
+
+route::get('/permissions/{permission}/assign', [PermissionController::class,'assign'])->name('permissions.assign');//to assign roles
+route::post('/permissions/{permission}/', [PermissionController::class,'sync'])->name('permissions.sync');//to assign roles (post)
+
+route::get('/roles/{role}/assign', [RoleController::class,'assign'])->name('roles.assign');//to assign Permission
+route::post('/roles/{role}/', [RoleController::class,'sync'])->name('roles.sync');//to assign permission (post)
+
+
+route::get('/users/{user}/assignrole',[UserController::class, 'assignrole'])->name('users.assignrole');//assign users to role
+route::post('/users/{user}/role',[UserController::class, 'syncrole'])->name('users.syncrole');//assign users to role
+
+route::get('/users/{user}/assignpermission',[UserController::class, 'assignpermission'])->name('users.assignpermission');//assign users to role
+route::post('/users/{user}/permission',[UserController::class, 'syncpermission'])->name('users.syncpermission');//assign users to role
 require __DIR__.'/auth.php';
